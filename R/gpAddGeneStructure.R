b6cdaf0487798c60dd90aad63e4e997c429b1a96
Bernat Gel <bernatgel@gmail.com>
2018-11-03 15:55:20 +0100

    Fixed reference to non-existent vars (gp)

--- R/gpAddGeneStructure.R
+++ R/gpAddGeneStructure.R
@@ -33,13 +33,13 @@ gpAddGeneStructure <- function(genoplot, coding.height=1, non.coding.height=0.5,
 
 
 #Assume there's a "type" mcol in exons with coding non coding
-  coding.exons <- gp$exons[gp$exons$type=="coding"]
-  non.coding.exons <- gp$exons[gp$exons$type=="non.coding"]
-  introns <- gp$introns
+  coding.exons <- genoplot$exons[genoplot$exons$type=="coding"]
+  non.coding.exons <- genoplot$exons[genoplot$exons$type=="non.coding"]
+  introns <- genoplot$introns
   #TODO: enforce type in plotGene or deal with it here with some default
-  for(i in seq_len(length(gp$regions.kp))) {
-    karyoploteR::kpRect(gp$regions.kp[[i]], data=coding.exons, y0=(1-coding.height)/2, y1=1-(1-coding.height)/2, ymin=0, ymax=1, data.panel="ideogram", col=coding.exons.col, border=coding.exons.border, ...)
-    karyoploteR::kpRect(gp$regions.kp[[i]], data=non.coding.exons, y0=(1-non.coding.height)/2, y1=1-(1-non.coding.height)/2, ymin=0, ymax=1, data.panel="ideogram", col=non.coding.exons.col, border=non.coding.exons.border, ...)
-    karyoploteR::kpSegments(gp$regions.kp[[i]], data=introns, y0=0.5, y1=0.5, ymin=0, ymax=1, data.panel="ideogram", lwd=introns.lwd, col=instrons.col, ...)
+  for(i in seq_len(length(genoplot$regions.kp))) {
+    karyoploteR::kpRect(genoplot$regions.kp[[i]], data=coding.exons, y0=(1-coding.height)/2, y1=1-(1-coding.height)/2, ymin=0, ymax=1, data.panel="ideogram", col=coding.exons.col, border=coding.exons.border, ...)
+    karyoploteR::kpRect(genoplot$regions.kp[[i]], data=non.coding.exons, y0=(1-non.coding.height)/2, y1=1-(1-non.coding.height)/2, ymin=0, ymax=1, data.panel="ideogram", col=non.coding.exons.col, border=non.coding.exons.border, ...)
+    karyoploteR::kpSegments(genoplot$regions.kp[[i]], data=introns, y0=0.5, y1=0.5, ymin=0, ymax=1, data.panel="ideogram", lwd=introns.lwd, col=instrons.col, ...)
   }
 }
